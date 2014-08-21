7246620894b578cd8120669e2844db693d887804
KoenVanExem <koen@vanexem.be>
2014-08-21 19:29:41 +0200

    ok

---
+++ Adapters/Database/SqlShared/Extents/ExtentStatement.cs
@@ -0,0 +1,261 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExtentStatement.cs" company="Allors bvba">
+//   Copyright 2002-2013 Allors bvba.
+// 
+// Dual Licensed under
+//   a) the Lesser General Public Licence v3 (LGPL)
+//   b) the Allors License
+// 
+// The LGPL License is included in the file lgpl.txt.
+// The Allors License is an addendum to your contract.
+// 
+// Allors Platform is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// For more information visit http://www.allors.com/legal
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Allors.Adapters.Database.Sql
+{
+    using System.Collections;
+
+    using Allors.Meta;
+
+    public abstract class ExtentStatement
+    {
+        private readonly ArrayList referenceAssociationInstances;
+        private readonly ArrayList referenceAssociations;
+        private readonly ArrayList referenceRoleInstances;
+        private readonly ArrayList referenceRoles;
+
+        private readonly SqlExtent extent;
+
+        protected ExtentStatement(SqlExtent extent)
+        {
+            this.extent = extent;
+
+            this.referenceRoles = new ArrayList();
+            this.referenceAssociations = new ArrayList();
+
+            this.referenceRoleInstances = new ArrayList();
+            this.referenceAssociationInstances = new ArrayList();
+        }
+
+        public SqlExtent Extent
+        {
+            get { return this.extent; }
+        }
+
+        public abstract bool IsRoot { get; }
+
+        public Schema Schema
+        {
+            get { return this.Session.SqlDatabase.Schema; }
+        }
+
+        public ExtentSort Sorter
+        {
+            get { return this.extent.Sorter; }
+        }
+
+        protected DatabaseSession Session
+        {
+            get { return this.extent.Session; }
+        }
+
+        protected ObjectType Type
+        {
+            get { return this.extent.ObjectType; }
+        }
+
+        public void AddJoins(ObjectType rootClass, string alias)
+        {
+            foreach (RoleType role in this.referenceRoles)
+            {
+                var relationType = role.RelationTypeWhereRoleType;
+                var association = relationType.AssociationType;
+
+                if (!role.ObjectType.IsUnit)
+                {
+                    if ((association.IsMany && role.IsMany) || !relationType.ExistExclusiveRootClasses)
+                    {
+                        this.Append(" LEFT OUTER JOIN " + this.Schema.Table(role) + " " + role.RootName + "_R");
+                        this.Append(" ON " + alias + "." + this.Schema.ObjectId + "=" + role.RootName + "_R." + this.Schema.AssociationId);
+                    }
+                    else
+                    {
+                        if (role.IsMany)
+                        {
+                            this.Append(" LEFT OUTER JOIN " + this.Schema.Table(role.ObjectType.ExclusiveRootClass) + " " + role.RootName + "_R");
+                            this.Append(" ON " + alias + "." + this.Schema.ObjectId + "=" + role.RootName + "_R." + this.Schema.Column(association));
+                        }
+                    }
+                }
+            }
+
+            foreach (RoleType role in this.referenceRoleInstances)
+            {
+                var relationType = role.RelationTypeWhereRoleType;
+
+                if (!role.ObjectType.IsUnit && role.IsOne)
+                {
+                    if (!relationType.ExistExclusiveRootClasses)
+                    {
+                        this.Append(" LEFT OUTER JOIN " + this.Schema.Objects + " " + this.GetJoinName(role));
+                        this.Append(" ON " + this.GetJoinName(role) + "." + this.Schema.ObjectId + "=" + role.RootName + "_R." + this.Schema.RoleId + " ");
+                    }
+                    else
+                    {
+                        this.Append(" LEFT OUTER JOIN " + this.Schema.Objects + " " + this.GetJoinName(role));
+                        this.Append(" ON " + this.GetJoinName(role) + "." + this.Schema.ObjectId + "=" + alias + "." + this.Schema.Column(role) + " ");
+                    }
+                }
+            }
+
+            foreach (AssociationType association in this.referenceAssociations)
+            {
+                var relationType = association.RelationTypeWhereAssociationType;
+                var role = relationType.RoleType;
+
+                if ((association.IsMany && role.IsMany) || !relationType.ExistExclusiveRootClasses)
+                {
+                    this.Append(" LEFT OUTER JOIN " + Schema.Table(association) + " " + association.RootName + "_A");
+                    this.Append(" ON " + alias + "." + this.Schema.ObjectId + "=" + association.RootName + "_A." + this.Schema.RoleId);
+                }
+                else
+                {
+                    if (!role.IsMany)
+                    {
+                        this.Append(" LEFT OUTER JOIN " + Schema.Table(association.ObjectType.ExclusiveRootClass) + " " + association.RootName + "_A");
+                        this.Append(" ON " + alias + "." + this.Schema.ObjectId + "=" + association.RootName + "_A." + Schema.Column(role));
+                    }
+                }
+            }
+
+            foreach (AssociationType association in this.referenceAssociationInstances)
+            {
+                var relationType = association.RelationTypeWhereAssociationType;
+                var role = relationType.RoleType;
+
+                if (!association.ObjectType.IsUnit && association.IsOne)
+                {
+                    if (!relationType.ExistExclusiveRootClasses)
+                    {
+                        this.Append(" LEFT OUTER JOIN " + this.Schema.Objects + " " + this.GetJoinName(association));
+                        this.Append(" ON " + this.GetJoinName(association) + "." + this.Schema.ObjectId + "=" + association.RootName + "_A." + this.Schema.AssociationId + " ");
+                    }
+                    else
+                    {
+                        if (role.IsOne)
+                        {
+                            this.Append(" LEFT OUTER JOIN " + this.Schema.Objects + " " + this.GetJoinName(association));
+                            this.Append(" ON " + this.GetJoinName(association) + "." + this.Schema.ObjectId + "=" + association.RootName + "_A." + this.Schema.ObjectId + " ");
+                        }
+                        else
+                        {
+                            this.Append(" LEFT OUTER JOIN " + this.Schema.Objects + " " + this.GetJoinName(association));
+                            this.Append(" ON " + this.GetJoinName(association) + "." + this.Schema.ObjectId + "=" + alias + "." + Schema.Column(association) + " ");
+                        }
+                    }
+                }
+            }
+        }
+
+        public abstract string AddParameter(object obj);
+
+        public bool AddWhere(ObjectType rootClass, string alias)
+        {
+            var useWhere = !this.Extent.ObjectType.ExistExclusiveRootClass || !this.Extent.ObjectType.ExclusiveRootClass.ExistExclusiveConcreteSubclass;
+            
+            if (useWhere)
+            {
+                this.Append(" WHERE ( ");
+                if (!this.Type.IsInterface && !this.Type.IsAbstract)
+                {
+                    this.Append(" " + alias + "." + this.Schema.TypeId + "=" + this.AddParameter(this.Type.Id));
+                    foreach (var subClass in this.Type.Subclasses)
+                    {
+                        this.Append(" OR " + alias + "." + this.Schema.TypeId + "=" + this.AddParameter(subClass.Id));
+                    }
+                }
+                else
+                {
+                    var first = true;
+                    foreach (var subClass in this.Type.Subclasses)
+                    {
+                        if (subClass.IsConcrete)
+                        {
+                            if (first)
+                            {
+                                first = false;
+                            }
+                            else
+                            {
+                                this.Append(" OR ");
+                            }
+
+                            this.Append(" " + alias + "." + this.Schema.TypeId + "=" + this.AddParameter(subClass.Id));
+                        }
+                    }
+                }
+
+                this.Append(" ) ");
+            }
+
+            return useWhere;
+        }
+
+        public abstract void Append(string part);
+
+        public abstract string CreateAlias();
+
+        public abstract ExtentStatement CreateChild(SqlExtent extent, AssociationType association);
+
+        public abstract ExtentStatement CreateChild(SqlExtent extent, RoleType role);
+
+        public string GetJoinName(AssociationType association)
+        {
+            return association.FullSingularName + "_AC";
+        }
+
+        public string GetJoinName(RoleType role)
+        {
+            return role.FullSingularName + "_RC";
+        }
+
+        public void UseAssociation(AssociationType association)
+        {
+            if (!association.ObjectType.IsUnit && !this.referenceAssociations.Contains(association))
+            {
+                this.referenceAssociations.Add(association);
+            }
+        }
+
+        public void UseAssociationInstance(AssociationType association)
+        {
+            if (!this.referenceAssociationInstances.Contains(association))
+            {
+                this.referenceAssociationInstances.Add(association);
+            }
+        }
+
+        public void UseRole(RoleType role)
+        {
+            if (!role.ObjectType.IsUnit && !this.referenceRoles.Contains(role))
+            {
+                this.referenceRoles.Add(role);
+            }
+        }
+
+        public void UseRoleInstance(RoleType role)
+        {
+            if (!this.referenceRoleInstances.Contains(role))
+            {
+                this.referenceRoleInstances.Add(role);
+            }
+        }
+    }
+}
---
+++ Adapters/Database/SqlShared/Predicates/AssociationContainedInEnumerable.cs
@@ -0,0 +1,85 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssociationContainedInEnumerable.cs" company="Allors bvba">
+//   Copyright 2002-2013 Allors bvba.
+// 
+// Dual Licensed under
+//   a) the Lesser General Public Licence v3 (LGPL)
+//   b) the Allors License
+// 
+// The LGPL License is included in the file lgpl.txt.
+// The Allors License is an addendum to your contract.
+// 
+// Allors Platform is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// For more information visit http://www.allors.com/legal
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Allors.Adapters.Database.Sql
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    using Meta;
+
+    public sealed class AssociationContainedInEnumerable : In
+    {
+        private readonly AssociationType association;
+        private readonly IEnumerable<IObject> enumerable;
+
+        public AssociationContainedInEnumerable(ExtentFiltered extent, AssociationType association, IEnumerable<IObject> enumerable)
+        {
+            extent.CheckAssociation(association);
+            CompositePredicateAssertions.AssertAssociationContainedIn(association, this.enumerable);
+            this.association = association;
+            this.enumerable = enumerable;
+        }
+
+        public override bool BuildWhere(ExtentStatement statement, string alias)
+        {
+            var schema = statement.Schema;
+
+            var inStatement = new StringBuilder("0");
+            foreach (var inObject in this.enumerable)
+            {
+                inStatement.Append(",");
+                inStatement.Append(inObject.Id.ToString());
+            }
+
+            if ((this.association.IsMany && this.association.RelationTypeWhereAssociationType.RoleType.IsMany) || !this.association.RelationTypeWhereAssociationType.ExistExclusiveRootClasses)
+            {
+                statement.Append(" (" + this.association.RootName + "_A." + schema.AssociationId + " IS NOT NULL AND ");
+                statement.Append(" " + this.association.RootName + "_A." + schema.AssociationId + " IN (\n");
+                statement.Append(inStatement.ToString());
+                statement.Append(" ))\n");
+            }
+            else
+            {
+                if (this.association.RelationTypeWhereAssociationType.RoleType.IsMany)
+                {
+                    statement.Append(" (" + alias + "." + schema.Column(this.association) + " IS NOT NULL AND ");
+                    statement.Append(" " + alias + "." + schema.Column(this.association) + " IN (\n");
+                    statement.Append(inStatement.ToString());
+                    statement.Append(" ))\n");
+                }
+                else
+                {
+                    statement.Append(" (" + this.association.RootName + "_A." + schema.ObjectId + " IS NOT NULL AND ");
+                    statement.Append(" " + this.association.RootName + "_A." + schema.ObjectId + " IN (\n");
+                    statement.Append(inStatement.ToString());
+                    statement.Append(" ))\n");
+                }
+            }
+
+            return this.Include;
+        }
+
+        public override void Setup(ExtentStatement statement)
+        {
+            statement.UseAssociation(this.association);
+        }
+    }
+}
