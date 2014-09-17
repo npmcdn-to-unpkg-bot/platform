72a36fe2f0a950f5ef79e229cf9c94a2144a3e4c
KoenVanExem <koen@vanexem.be>
2014-09-17 14:29:06 +0200

    ok

--- Core/Generate.cmd
+++ Core/Generate.cmd
@@ -1,6 +1,6 @@
 @SET PATH=%PATH%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319;C:\Windows\Microsoft.NET\Framework\v4.0.30319
 
-@msbuild Adapters.sln /target:Meta:Rebuild /p:Configuration="Debug" /verbosity:minimal
+@msbuild Core.sln /target:Meta:Rebuild /p:Configuration="Debug" /verbosity:minimal
 @msbuild Meta\Generate.proj /verbosity:minimal
 
 @if NOT ["%errorlevel%"]==["0"] (
