d46314501c0f7c17376b3fdff24ec1bd68231ac9
KoenVanExem <koen@vanexem.be>
2014-11-30 16:22:49 +0100

    ok

--- Base/Console/Program.cs
+++ Base/Console/Program.cs
@@ -11,8 +11,9 @@
 
         public static void Generate()
         {
-            Development.Repository.Tasks.Generate.Execute("../../../../Core/Templates/domain.cs.stg", "../../../Domain/Generated/Core");
-            Development.Repository.Tasks.Generate.Execute("../../../Templates/domain.cs.stg", "../../../Domain/Generated/Base");
+            //Development.Repository.Tasks.Generate.Execute("../../../../Core/Templates/domain.cs.stg", "../../../Domain/Generated/Core");
+            Development.Repository.Tasks.Generate.Execute("../../../../Core/Templates/repository.stg", "../../../Repository/Generated");
+            //Development.Repository.Tasks.Generate.Execute("../../../Templates/domain.cs.stg", "../../../Domain/Generated/Base");
             Console.WriteLine("Finished");
             Console.ReadKey();
         }
