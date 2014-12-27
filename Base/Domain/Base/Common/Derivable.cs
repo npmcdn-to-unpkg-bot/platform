f04f7693b2c4ba4d878fd5a6203e1ffacd3df1e6
KoenVanExem <koen@vanexem.be>
2014-12-27 17:08:03 +0100

    ok

--- Base/Domain/Base/Common/Derivable.cs
+++ Base/Domain/Base/Common/Derivable.cs
@@ -22,12 +22,13 @@ namespace Allors.Domain
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using Allors.Meta;
 
     public static partial class DerivableExtensions
     {
-        private static Dictionary<Type, RoleType[]> requiredRoleTypesByClass; 
+        private static readonly Dictionary<string, RoleType[]> RequiredRoleTypesByClassName = new Dictionary<string, RoleType[]>(); 
 
         public static void BasePrepareDerivation(this Derivable @this, DerivablePrepareDerivation method)
         {
@@ -47,14 +48,20 @@ namespace Allors.Domain
             var derivation = method.Derivation;
             var @class = (Class)@this.Strategy.ObjectType;
 
-            // TODO: Optimize
-            foreach (var concreteRoleType in @class.ConcreteRoleTypes)
+            RoleType[] requiredRoleTypes;
+            if (!RequiredRoleTypesByClassName.TryGetValue(@class.Name, out requiredRoleTypes))
             {
-                if (concreteRoleType.IsRequired)
-                {
-                    var roleType = concreteRoleType.RoleType;
-                    derivation.Log.AssertExists(@this, roleType);
-                }
+                requiredRoleTypes = @class.ConcreteRoleTypes
+                    .Where(concreteRoleType => concreteRoleType.IsRequired)
+                    .Select(concreteRoleType => concreteRoleType.RoleType)
+                    .ToArray();
+
+                RequiredRoleTypesByClassName[@class.Name] = requiredRoleTypes;
+            }
+
+            foreach (var roleType in requiredRoleTypes)
+            {
+                derivation.Log.AssertExists(@this, roleType);
             }
         }
     }
