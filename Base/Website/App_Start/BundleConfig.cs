d40be70f8e86982c02a44e2f6de95be713d01e19
Koen Van Exem <koen@vanexem.be>
2015-05-17 13:10:50 +0200

    ok

--- Base/Website/App_Start/BundleConfig.cs
+++ Base/Website/App_Start/BundleConfig.cs
@@ -4,35 +4,32 @@
 
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js/default/pre").Include(
+                        "~/Scripts/modernizr-*",
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/js/default/post").Include(
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/moment-with-locales.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootstrap-select.js",
+                        "~/Scripts/bootstrap-datetimepicker.js",
+                        "~/Scripts/jasny-bootstrap.js",
+                        "~/Scripts/tinymce/tinymce.js",
+                        "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css/default").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap-select.css",
+                        "~/Content/bootstrap-datetimepicker.css",
+                        "~/Content/jasny-bootstrap.css",
+                        "~/Content/site.css"));
+
+            // Used by Identity
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
-                        "~/Scripts/moment-with-locales.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap-select.js",
-                      "~/Scripts/bootstrap-datetimepicker.js",
-                      "~/Scripts/jasny-bootstrap.js",
-                      "~/Scripts/tinymce/tinymce.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-select.css",
-                      "~/Content/bootstrap-datetimepicker.css",
-                      "~/Content/jasny-bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Scripts/jquery.validate*"));
         }
     }
 }
