0a768788a88ac4c38a9b632e9070fe24664b9aa2
Koen Van Exem <koen@vanexem.be>
2016-01-07 21:04:54 +0100

    ok

---
+++ Base/Generate.cmd
@@ -0,0 +1,8 @@
+@SET PATH=%PATH%;C:\Program Files\MSBuild\14.0\Bin;C:\Program Files (x86)\MSBuild\14.0\Bin;C:\Windows\Microsoft.NET\Framework64\v4.0.30319;C:\Windows\Microsoft.NET\Framework\v4.0.30319
+
+msbuild Base.sln /target:Clean /verbosity:minimal
+msbuild Base.sln /target:Allors_Tools_Cmd:Rebuild /p:Configuration="Debug" /verbosity:minimal
+
+..\Tools\Allors.Tools.Cmd\bin\debug\Allors.Tools.Cmd.exe repository generate base.sln repository templates/meta.cs.stg meta/generated
+
+@pause
