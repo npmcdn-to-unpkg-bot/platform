41bc2325fb08f67efcc76cdc043914cb517ad92b
Koen Van Exem <koen@vanexem.be>
2016-04-16 14:15:03 +0200

    sorting on select

--- Base/Desktop/Allors/Client/Base/Angular/components/bootstrap/Select.ts
+++ Base/Desktop/Allors/Client/Base/Angular/components/bootstrap/Select.ts
@@ -7,28 +7,28 @@
             return `
 <ui-select ng-if="$ctrl.roleType.isOne && $ctrl.options !== undefined" ng-model="$ctrl.role" ng-disabled="!$ctrl.canWrite" ng-required="$ctrl.roleType.isRequired">
     <ui-select-match placeholder="Select a value" allow-clear="{{!$ctrl.roleType.isRequired}}">{{$ctrl.displayValue}}</ui-select-match>
-    <ui-select-choices repeat="item in $ctrl.options | filter: $ctrl.filterFunction($select.search)">
+    <ui-select-choices repeat="item in $ctrl.options | filter: $ctrl.filterFunction($select.search) | orderBy: $ctrl.orderBy()">
         <div ng-bind-html="item[$ctrl.display] | highlight: $select.search"></div>
     </ui-select-choices>
 </ui-select>
 
 <ui-select ng-if="$ctrl.roleType.isOne && $ctrl.options === undefined" ng-model="$ctrl.role" ng-disabled="!$ctrl.canWrite" ng-required="$ctrl.roleType.isRequired">
     <ui-select-match placeholder="Select a value" allow-clear="{{!$ctrl.roleType.isRequired}}">{{$ctrl.displayValue}}</ui-select-match>
-    <ui-select-choices repeat="item in $ctrl.asyncOptions" refresh="$ctrl.refresh($select.search)" refresh-delay="0">
+    <ui-select-choices repeat="item in $ctrl.asyncOptions | orderBy: $ctrl.orderBy()" refresh="$ctrl.refresh($select.search)" refresh-delay="0">
         <div ng-bind-html="item[$ctrl.display] | highlight: $select.search"></div>
     </ui-select-choices>
 </ui-select>
 
 <ui-select ng-if="$ctrl.roleType.isMany && $ctrl.options !== undefined" multiple ng-model="$ctrl.role" ng-disabled="!$ctrl.canWrite" ng-required="$ctrl.roleType.isRequired">
     <ui-select-match placeholder="Select values" allow-clear="{{!$ctrl.roleType.isRequired}}">{{$item[$ctrl.display]}}</ui-select-match>
-    <ui-select-choices repeat="item in $ctrl.options | filter: $ctrl.filterFunction($select.search)">
+    <ui-select-choices repeat="item in $ctrl.options | filter: $ctrl.filterFunction($select.search) | orderBy: $ctrl.orderBy()">
         <div ng-bind-html="item[$ctrl.display] | highlight: $select.search"></div>
     </ui-select-choices>
 </ui-select>
 
 <ui-select ng-if="$ctrl.roleType.isMany && $ctrl.options === undefined" multiple ng-model="$ctrl.role" ng-disabled="!$ctrl.canWrite" ng-required="$ctrl.roleType.isRequired">
     <ui-select-match placeholder="Select values" allow-clear="{{!$ctrl.roleType.isRequired}}">{{$item[$ctrl.display]}}</ui-select-match>
-    <ui-select-choices repeat="item in $ctrl.asyncOptions" refresh="$ctrl.refresh($select.search)" refresh-delay="0">
+    <ui-select-choices repeat="item in $ctrl.asyncOptions | orderBy: $ctrl.orderBy()" refresh="$ctrl.refresh($select.search)" refresh-delay="0">
         <div ng-bind-html="item[$ctrl.display] | highlight: $select.search"></div>
     </ui-select-choices>
 </ui-select>
@@ -46,17 +46,21 @@
             relation: "@",
             display: "@",
             options: "<",
+            order: "<",
             lookup: "&lookup"
         }
 
         options: SessionObject[];
         asyncOptions: SessionObject[];
+        order: any;
+
+        remove: () => void;
 
         static $inject = ["$log", "$translate"];
         constructor($log: angular.ILogService, $translate: angular.translate.ITranslateService) {
             super($log, $translate);
         }
-        
+
         filterFunction(criteria: string): (object) => boolean {
 
             return (object) => {
@@ -69,6 +73,15 @@
                 }
 
                 return false;
+            }
+        }
+
+        orderBy() {
+
+            if (this.order) {
+                return this.order;
+            } else {
+                return this.display;
             }
         }
 
