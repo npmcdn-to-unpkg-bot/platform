bfbae8275fbf4dbf5ce8df032ef6d2d70a83a57a
Koen Van Exem <koen@vanexem.be>
2016-05-29 22:39:05 +0200

    github

--- Base/Desktop/Allors/Client/Custom/Person.ts
+++ Base/Desktop/Allors/Client/Custom/Person.ts
@@ -6,26 +6,27 @@ namespace Allors.Domain
         displayName;
     }
 
-    Person.prototype.hello = function() {
-        return `Hello ${this.displayName}`;
-    }
-
-    Object.defineProperty(Person.prototype, "displayName", {
-        get: function() {
-            if (this.FirstName || this.LastName) {
-                if (this.FirstName && this.LastName) {
-                    return this.FirstName + " " + this.LastName;
-                } else if (this.LastName) {
-                    return this.LastName;
-                } else {
-                    return this.FirstName;
-                }
-            }
-
-            if (this.UserName) {
-                return this.UserName;
-            }
-
-            return "N/A";
+    extend(Person, {
+        hello() {
+            return `Hello ${this.displayName}`;
         }
-    });}
+        ,
+        get displayName() {
+                if (this.FirstName || this.LastName) {
+                    if (this.FirstName && this.LastName) {
+                        return this.FirstName + " " + this.LastName;
+                    } else if (this.LastName) {
+                        return this.LastName;
+                    } else {
+                        return this.FirstName;
+                    }
+                }
+
+                if (this.UserName) {
+                    return this.UserName;
+                }
+
+                return "N/A";
+            }
+    });
+}
